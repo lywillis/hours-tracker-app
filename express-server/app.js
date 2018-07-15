a70ead21f6aa25b2c26b4056a9ed239e7b9a7921
Leah Willis <lywillis7686@gmail.com>
2018-07-14 17:24:25 -0700

    First commit

---
+++ express-server/app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import SourceMapSupport from 'source-map-support';
+
+// define our app using express
+const app = express();
+
+
+// allow-cors
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
+
+  // configure app
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set the port
+const port = process.env.PORT || 3001;
+// connect to database
+const mongodb = 'mongodb://express:express1@ds229621.mlab.com:29621/hourstracker';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodb, {
+    useNewUrlParser: true
+});
+
+// add Source Map Support
+SourceMapSupport.install();
+
+// catch 404
+app.use((req, res, next) => {
+    res.status(404).send('<h2 align=center>Page Not Found!</h2>');
+  });
+  // start the server
+  app.listen(port,() => {
+    console.log(`App Server Listening at ${port}`);
+  });
