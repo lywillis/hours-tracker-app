0a87c3c54c39ef636c2480c1c23e1ad6bf639de6
Leah Willis <lywillis7686@gmail.com>
2018-07-14 20:03:42 -0700

    Create mongoDB service

--- hours-tracker-app/src/app/app.module.ts
+++ hours-tracker-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MongoService } from 'src/app/services/mongo.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [MongoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
