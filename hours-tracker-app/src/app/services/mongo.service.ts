345ec8e7bcb38a94b82b252735c42b39a3211f99
Leah Willis <lywillis7686@gmail.com>
2018-07-15 12:25:12 -0700

    Implement GET request in mongo service

--- hours-tracker-app/src/app/services/mongo.service.ts
+++ hours-tracker-app/src/app/services/mongo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { TimeLog } from 'src/app/models/TimeLog';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,31 @@ export class MongoService {
   constructor(private http: Http) {
    }
 
-   createTimeLog(timeLog: any): Promise<any> {
+  createTimeLog(timeLog: TimeLog): Promise<TimeLog> {
     return this.http.post(this.apiUrl, timeLog)
-    .toPromise()
-    .then(this.handleData)
-    .catch(this.handleError);
-   }
+      .toPromise()
+      .then(this.handleData)
+      .catch(this.handleError);
+  }
 
-   handleData() {}
+  getTimeLogs(): Promise<TimeLog[]> {
+    return this.http.get(this.apiUrl)
+      .toPromise()
+      .then(res => {
+        const body = this.handleData(res);
+        return body.logs;
+      })
+      .catch(this.handleError);
+  }
 
-   handleError() {}
+   private handleData(res: any) {
+    const body = res.json();
+    console.log(body); // for development purposes only
+    return body || {};
+}
+
+private handleError(error: any): Promise<any> {
+  console.error('An error occurred', error); // for development purposes only
+  return Promise.reject(error.message || error);
+}
 }
