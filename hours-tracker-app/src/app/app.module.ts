642a2b2170e116c2c1e71e0fb83b9dff96a301dd
Leah Willis <lywillis7686@gmail.com>
2018-07-15 22:49:37 -0700

    Create timer component

--- hours-tracker-app/src/app/app.module.ts
+++ hours-tracker-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TimeLogService } from 'src/app/services/timelog.service';
 import { LogListComponent } from './log/log-list/log-list.component';
+import { TimerComponent } from './timer/timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogListComponent
+    LogListComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
