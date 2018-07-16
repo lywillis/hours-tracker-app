1918d77b9391b0923f7b05b81b1a9a555b6363fa
Leah Willis <lywillis7686@gmail.com>
2018-07-15 21:30:41 -0700

    Create Duration model and add to TimeLog class

--- hours-tracker-app/src/app/models/TimeLog.ts
+++ hours-tracker-app/src/app/models/TimeLog.ts
@@ -1,10 +1,14 @@
+import { Duration } from 'src/app/models/Duration';
+
 export class TimeLog {
     _id: string;
     start: Date;
     end: Date;
+    duration: Duration;
 
     constructor(start: Date, end: Date) {
         this.start = start;
         this.end = end;
+        this.duration = new Duration(start, end);
     }
 }
