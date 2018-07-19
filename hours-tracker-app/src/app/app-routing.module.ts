3cf5a3b1343ece9cacf9dad4152089f6f8b473c8
Leah Willis <lywillis7686@gmail.com>
2018-07-18 17:54:46 -0700

    Add routing and create dashboard component

---
+++ hours-tracker-app/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TimerComponent } from 'src/app/timer/timer/timer.component';
+import { LogListComponent } from 'src/app/log/log-list/log-list.component';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', component: DashboardComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'logs', component: LogListComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- hours-tracker-app/src/app/app.module.ts
+++ hours-tracker-app/src/app/app.module.ts
@@ -7,19 +7,24 @@ import { TimeLogService } from 'src/app/services/timelog.service';
 import { LogListComponent } from './log/log-list/log-list.component';
 import { TimerComponent } from './timer/timer/timer.component';
 import { DurationPipe } from './pipes/duration.pipe';
+import { AppRoutingModule } from './/app-routing.module';
+import { TimerService } from 'src/app/services/timer.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogListComponent,
     TimerComponent,
-    DurationPipe
+    DurationPipe,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [TimeLogService],
+  providers: [TimeLogService, TimerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
